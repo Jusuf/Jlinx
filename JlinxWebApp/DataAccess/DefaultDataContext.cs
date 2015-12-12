125176ccc8fa07166d4d6e512c3cbe2b3cea44f7
jusuf_b <jusuf_b@hotmail.com>
2015-12-12 02:16:40 +0100

    Start

---
+++ JlinxWebApp/DataAccess/DefaultDataContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using JlinxWebApp.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace JlinxWebApp.DataAccess
+{
+    public class DefaultDataContext : DbContext
+    {
+        public DbSet<Link> Links { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ JlinxWebApp/Models/Link.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JlinxWebApp.Models
+{
+    public class Link
+    {
+        public int Id { get; set; }
+
+        public string LinkDescription { get; set; }
+
+        public string UrlAddress { get; set; }
+
+        public DateTime Created { get; set; }
+
+    }
+}
